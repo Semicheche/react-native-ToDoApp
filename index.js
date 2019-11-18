0a566e05a6b2d3aed9a29d535f5332eb6c3dd560
Luciano Semicheche <lucianosemicheche@gmail.com>
2019-11-18 08:04:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Schedule from './src/screens/Schedule'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Schedule);
